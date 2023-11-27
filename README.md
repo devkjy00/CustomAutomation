3843190b39d4629ac23d79d4b228136c11b25bd7
devkjy00 <devkjy00@gmail.com>
2023-11-27 22:33:57 +0900

    init spring boot

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # CustomAutomation
-"A project was developed for the dynamic management of data used in daily life. Dynamic data management logic was implemented using the Notion API."
+"A project was developed for the dynamic management of data used in daily life"
---
+++ src/main/java/jy/demo/CustomAutomationApplication.java
@@ -0,0 +1,13 @@
+package jy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomAutomationApplication.class, args);
+	}
+
+}
